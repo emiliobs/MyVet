9a91854096349e30492fb9567a4b49c1b6d932ba
emilio barrera <barrera_emilio@hotmail.com>
2019-08-08 11:55:57 +0100

    Creatre DataBase

---
+++ MyVet.Web/Data/Entities/Owner.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyVet.Web.Data.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Document { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [MaxLength(20)]
+        [Display(Name = "Fixed Phone")]
+        public string FixedPhone { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        [Display(Name = "Cell Phone")]
+        public string CellPhone { get; set; }
+
+        [MaxLength(100)]
+        public string Address { get; set; }
+
+        [Display(Name = "Owner Name")]
+        public string FullName => $"{FirstName} {LastName}";
+
+        [Display(Name = "Full Name With Document")]
+        public string FullNameWithDocument => $"{FirstName} - {LastName} - {Document}";
+    }
+}
