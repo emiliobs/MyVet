bab1f4eece867363e9b8a54dbf291008ff25b1ca
emilio barrera <barrera_emilio@hotmail.com>
2019-09-10 18:25:31 +0100

    Login Layout

--- MyVet.Web/Data/Entities/Pet.cs
+++ MyVet.Web/Data/Entities/Pet.cs
@@ -29,10 +29,10 @@ namespace MyVet.Web.Data.Entities
 
         public string Remarks { get; set; }
 
-        //TODO: replace the correct URL for the image
+       
         public string ImageFullPath => string.IsNullOrEmpty(ImageUrl)
             ? null 
-            : $"https://TDB.azurewebsites.net{ImageUrl.Substring(1)}";
+            : $"https://appmyvet.azurewebsites.net{ImageUrl.Substring(1)}";
 
         [Display(Name = "Born")]
        // [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
